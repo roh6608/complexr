ad751cf8a457dfde46d948f7054cfe80974da82d
roh6608 <72960901+roh6608@users.noreply.github.com>
2021-09-29 20:27:37 +1000

    Initial commit

---
+++ R/types.R
@@ -0,0 +1,25 @@
+#' Convert from polar to rectangular form
+#'
+#' @param mod The modulus of the complex number, or vector of moduli.
+#' @param arg The argument of the complex number or vector of arguments, in radians.
+#' @return The rectangular form of the complex number.
+#' @examples
+#' polar_rect(1,45)
+polar_rect = function(mod, arg){
+  x = mod*cos(arg)
+  y = mod*sin(arg)
+
+  return(complex(real = x, imaginary = y))
+}
+
+#' Convert from rectangular to polar form
+#'
+#' @param z A complex number or vector of complex numbers in rectangular form.
+#' @return The polar form of the complex number as a list, note the argument is in radians.
+#' @examples
+#' rect_polar(1+1i)
+rect_polar = function(z){
+  mod = Mod(z)
+  arg = Arg(z)
+  return(list(mod, arg))
+}
