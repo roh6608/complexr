26c0e892ef5aca7c94f39ce6bdce0a6d5bfe9aa4
roh6608 <72960901+roh6608@users.noreply.github.com>
2021-09-30 11:56:13 +1000

    Added unit option for complex number type conversions

--- R/types.R
+++ R/types.R
@@ -1,13 +1,25 @@
 #' Convert from polar to rectangular form
 #'
 #' @param mod The modulus of the complex number, or vector of moduli.
-#' @param arg The argument of the complex number or vector of arguments, in radians.
+#' @param arg The argument of the complex number or vector of arguments.
+#' @param ang The units for the argument of the complex number, can either be rad or deg, defaults to rad.
 #' @return The rectangular form of the complex number.
 #' @examples
-#' polar_rect(1,pi/4)
-polar_rect = function(mod, arg){
-  x = mod*cos(arg)
-  y = mod*sin(arg)
+#' polar_rect(1,pi/4,"rad")
+polar_rect = function(mod, arg, ang = "rad"){
+  if(ang == "rad"){
+    x = mod*cos(arg)
+    y = mod*sin(arg)
+
+  } else if(ang == "deg"){
+    x = mod*cos(deg_rad(arg))
+    y = mod*sin(deg_rad(arg))
+
+  } else{
+    warning("Valid string for ang was not given, defaulting to radians")
+    x = mod*cos(arg)
+    y = mod*sin(arg)
+  }
 
   return(complex(real = x, imaginary = y))
 }
@@ -15,11 +27,25 @@ polar_rect = function(mod, arg){
 #' Convert from rectangular to polar form
 #'
 #' @param z A complex number or vector of complex numbers in rectangular form.
-#' @return The polar form of the complex number as a list, note the argument is in radians.
+#' @param ang The units for the output, can either be rad or deg, defaults to rad.
+#' @return The polar form of the complex number as a list, note the units are as specified by ang.
 #' @examples
-#' rect_polar(1+1i)
-rect_polar = function(z){
-  mod = Mod(z)
-  arg = Arg(z)
-  return(list(mod, arg))
+#' rect_polar(1+1i, "deg")
+rect_polar = function(z, ang = "rad"){
+  if(ang == "rad"){
+    mod = Mod(z)
+    arg = Arg(z)
+    return(list(mod, arg))
+
+  } else if(ang == "deg"){
+    mod = Mod(z)
+    arg = Arg(z)
+    return(list(mod, rad_deg(arg)))
+  } else{
+    warning("Valid string for ang was not given, defaulting to radians")
+    mod = Mod(z)
+    arg = Arg(z)
+    return(list(mod, arg))
+  }
+
 }
